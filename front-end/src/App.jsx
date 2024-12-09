9823fc74a5c8475ae75e182b72041c7043e80e4a
srinu <srinusrinu0299@gmail.com>
2024-12-09 15:47:58 +0530

    filename chnages

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -57,8 +57,8 @@ import { Auth0Provider } from '@auth0/auth0-react';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import First from './components/first';
-import Login from './components/Login'; // Login page
-import Home from './components/Home'; // Home page
+import Login from './components/login'; // Login page
+import Home from './components/home'; // Home page
 import Details from './components/itemsDetails'; // Details page
 
 const App = () => {
